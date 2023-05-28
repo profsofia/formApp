17da1acc7f7fd1b0220693b5377d5ca1f1aaad94
sofia <sofiainesschenone@gmail.com>
2023-05-28 10:18:33 -0300

    Terminando la carga de los módulos, y las rutas principales

--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReactiveRoutingModule } from './reactive-routing.module';
+import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
+import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
+import { BasicPageComponent } from './pages/basic-page/basic-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DynamicPageComponent, SwitchesPageComponent, BasicPageComponent
+  ],
   imports: [
     CommonModule,
     ReactiveRoutingModule
